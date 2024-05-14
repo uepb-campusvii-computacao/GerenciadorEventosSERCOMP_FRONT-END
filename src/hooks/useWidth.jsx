d8af5a9ec005756dc3ba43c624a1b81377102b9a
TidusWR <harllemnascimento@gmail.com>
2024-05-13 22:28:29 -0300

    melhora os dados retornados para o dashboard da página Home(Admin)

---
+++ src/hooks/useWidth.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+function useWidth() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return width;
+}
+
+export default useWidth;
