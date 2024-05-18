1de672c937e622aea8ea209713209b170d33a3ac
Yuri Monteiro <yuri.mont1043@gmail.com>
2024-05-18 19:34:54 -0300

    style: mudanca na flag do status de pagamento

--- src/components/AdminModule/Tables/InscritosTable.jsx
+++ src/components/AdminModule/Tables/InscritosTable.jsx
@@ -246,7 +246,7 @@ const InscritosTable = ({ data }) => {
                     <span className="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
                       {item.paymentStatus}
                     </span>
-                  ) : item.paymentStatus === "PENDENTE" ? (
+                  ) : item.paymentStatus === "PENDENTE" || item.paymentStatus === "EXPIRADO" ? (
                     <span className="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">
                       {item.paymentStatus}
                     </span>
